feb24a4d158f284a66e38a0742ea9e39bf8281ef
Bruno Soares <soares_de_faria@hotmail.com>
2023-04-23 14:41:44 -0300

    colocando os inputs em 100% de novo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -96,7 +96,7 @@ const Login = () => {
                 </label>
                 <input
                   {...register("email", { required: true })}
-                  className="shadow appearance-none border rounded w-[90%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="username"
                   type="text"
                   placeholder="Email"
@@ -116,7 +116,7 @@ const Login = () => {
                 </label>
                 <input
                   {...register("password", { required: true })}
-                  className="shadow appearance-none border rounded w-[90%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   type="password"
                   placeholder="********"
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -55,7 +55,7 @@ const SignUp = () => {
                 </label>
                 <input
                   {...register("email", { required: true })}
-                  className="shadow appearance-none border rounded w-[90%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="username"
                   type="text"
                   placeholder="Email"
@@ -75,7 +75,7 @@ const SignUp = () => {
                 </label>
                 <input
                   {...register("password", { required: true })}
-                  className="shadow appearance-none border rounded w-[90%] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                   id="password"
                   type="password"
                   placeholder="********"
